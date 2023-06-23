bbec1ec9c97a2fcc578eff586e9fa6108eabc889
Lucky Jain <luckyjain78281@gmail.com>
2023-06-23 21:35:30 +0530

    New App New Project

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import ExploreScreen from './screens/ExploreScreen';
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      <StatusBar style="dark" />
+      <Tab.Navigator>
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => <Icon name="home" size={32} color={focused ? "#2E7D32" : "#404040"} />,
+            tabBarLabel: ({ focused, color }) => <Text style={{ fontSize: 10, color: focused ? "#2E7D32" : "#404040" }}>Home</Text>,
+            headerShown: false,
+          }}
+          name="Home" component={HomeScreen} />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => <Icon name="arm-flex" size={32} color={focused ? "#2E7D32" : "#404040"} />,
+            tabBarLabel: ({ focused, color }) => <Text style={{ fontSize: 10, color: focused ? "#2E7D32" : "#404040" }}>Explore</Text>,
+            headerShown: false,
+          }}
+          name="Explore" component={ExploreScreen} />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused, color, size }) => <Icon name="account" size={32} color={focused ? "#2E7D32" : "#404040"} />,
+            tabBarLabel: ({ focused, color }) => <Text style={{ fontSize: 10, color: focused ? "#2E7D32" : "#404040" }}>Profile</Text>,
+            headerShown: false,
+          }}
+          name="Profile" component={ProfileScreen} />
+
+      </Tab.Navigator>
+
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +48,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bottomNavText: {
+    fontSize: 10
+  }
 });
